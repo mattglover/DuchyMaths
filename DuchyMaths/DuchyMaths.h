7f6c71bdfd4a9d35407b3b9981316cf9fe475fa8
Matt Glover <matt@duchysoftware.com>
2015-05-29 20:22:04 +0100

    Initial Commit

---
+++ DuchyMaths/DuchyMaths.h
@@ -0,0 +1,19 @@
+//
+//  DuchyMaths.h
+//  DuchyMaths
+//
+//  Created by Matt Glover on 29/05/2015.
+//  Copyright (c) 2015 Duchy Software Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DuchyMaths.
+FOUNDATION_EXPORT double DuchyMathsVersionNumber;
+
+//! Project version string for DuchyMaths.
+FOUNDATION_EXPORT const unsigned char DuchyMathsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DuchyMaths/PublicHeader.h>
+
+
