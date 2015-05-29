2dad4672501f51c3750ef170211906a4b598c80b
Matt Glover <matt@duchysoftware.com>
2015-05-29 20:27:22 +0100

    Created very basic class and method

---
+++ DuchyMaths/DuchyCalculator.swift
@@ -0,0 +1,16 @@
+//
+//  DuchyCalculator.swift
+//  DuchyMaths
+//
+//  Created by Matt Glover on 29/05/2015.
+//  Copyright (c) 2015 Duchy Software Ltd. All rights reserved.
+//
+
+import Foundation
+
+class DuchyCalculator {
+    
+    func multiplyBy7(value: Int) -> Int {
+        return value * 7
+    }
+}
